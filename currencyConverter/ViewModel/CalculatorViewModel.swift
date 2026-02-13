7c5ffb66f4a7c9a8606e2dfaf7f45f98ad8b0116
ybin4548 <ybin4548@gmail.com>
2026-02-13 16:11:00 +0900

    ✨feat: 환율 계산 예외처리 추가

--- currencyConverter/ViewModel/CalculatorViewModel.swift
+++ currencyConverter/ViewModel/CalculatorViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by 손영빈 on 2/13/26.
 //
 
-//TODO: 음수일 때 에러처리
+//TODO: 0,음수일 때 에러처리
 
 import Foundation
 
@@ -27,6 +27,10 @@ class CalculatorViewModel {
         }
         guard let amount = Double(text) else {
             showError?("올바른 숫자를 입력해주세요")
+            return
+        }
+        if amount <= 0 {
+            showError?("금액은 1보다 작을 수 없습니다.")
             return
         }
         let convertedValue = amount * item.rate

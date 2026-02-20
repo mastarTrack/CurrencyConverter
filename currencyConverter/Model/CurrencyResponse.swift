29482c907cb489cf2dbaf295cbebce8b4eb8d77a
ybin4548 <ybin4548@gmail.com>
2026-02-20 12:45:26 +0900

    ♻️ refactor: 업데이트 날짜 저장용 unix 추가

--- currencyConverter/Model/CurrencyResponse.swift
+++ currencyConverter/Model/CurrencyResponse.swift
@@ -9,4 +9,10 @@ import Foundation
 
 struct CurrencyResponse: Codable {
     let rates: [String: Double]
+    let unix: Int64 // 최근 업데이트 받은 정보
+    
+    enum CodingKeys: String, CodingKey {
+        case rates
+        case unix = "time_last_update_unix"
+    }
 }

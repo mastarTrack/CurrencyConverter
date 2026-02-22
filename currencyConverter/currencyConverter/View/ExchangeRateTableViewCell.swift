975c8caccee6beeca52a4ed60dd30d5d8dfda651
coduhee <vdyjb@ewha.ac.kr>
2026-02-22 21:40:54 +0900

    ✨feat: LV.7 즐겨찾기 버튼 구현

--- currencyConverter/currencyConverter/View/ExchangeRateTableViewCell.swift
+++ currencyConverter/currencyConverter/View/ExchangeRateTableViewCell.swift
@@ -24,20 +24,37 @@ class ExchangeRateTableViewCell: UITableViewCell {
     
     // 환율
     private let currencyLabel = UILabel().then {
-        $0.font = .systemFont(ofSize: 16, weight: .medium)
+        $0.font = .systemFont(ofSize: 16, weight: .regular)
         $0.textColor = .black
         $0.textAlignment = .right
     }
+    
     // 국가명
     private let countryLabel = UILabel().then {
         $0.font = .systemFont(ofSize: 14, weight: .regular)
         $0.textColor = .gray
     }
+    
     // 통화 코드 + 국가명 스택뷰
     private let labelStackView = UIStackView().then {
         $0.axis = .vertical
         $0.spacing = 4
     }
+    
+    // 즐겨찾기 버튼 구현
+    private let favoriteButton = UIButton().then {
+        let config = UIImage.SymbolConfiguration(pointSize: 25, weight: .regular)
+        // SF symbol
+        let image = UIImage(systemName: "star", withConfiguration: config)
+        $0.setImage(image, for: .normal)
+        $0.tintColor = .systemYellow
+        $0.addTarget(self, action: #selector(favoriteButtonTapped), for: .touchUpInside)
+    }
+    
+    
+    // MARK: -- View 이벤트 전달
+    // favoriteButton 클로저
+    var tappedfavoriteButton: (() -> Void)?
     
     
     // MARK: -- 초기화
@@ -56,7 +73,7 @@ class ExchangeRateTableViewCell: UITableViewCell {
         
         [codeLabel, countryLabel].forEach { labelStackView.addArrangedSubview($0)}
         
-        [labelStackView, currencyLabel].forEach { contentView.addSubview($0) }
+        [labelStackView, currencyLabel, favoriteButton].forEach { contentView.addSubview($0) }
         
         contentView.snp.makeConstraints {
             $0.edges.equalToSuperview()
@@ -69,14 +86,26 @@ class ExchangeRateTableViewCell: UITableViewCell {
         }
         
         currencyLabel.snp.makeConstraints {
-            $0.trailing.equalToSuperview().inset(16)
+            $0.trailing.equalTo(favoriteButton.snp.leading).offset(-10)
             $0.centerY.equalToSuperview()
             $0.leading.greaterThanOrEqualTo(labelStackView.snp.trailing).offset(16)
             $0.width.equalTo(120)
         }
         
+        favoriteButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(20)
+            $0.centerY.equalToSuperview()
+        }
+        
     }
     
+    
+    // MARK: -- @objc 메서드
+    @objc
+    private func favoriteButtonTapped() {
+        tappedfavoriteButton?() // 클로저 전달
+    }
+
     
     // MARK: -- text 데이터 대입 메서드
     func configure(code: String, rate: String, country: String) {
@@ -84,4 +113,10 @@ class ExchangeRateTableViewCell: UITableViewCell {
         currencyLabel.text = rate
         countryLabel.text = country
     }
+}
+
+
+#Preview {
+    let dummyViewModel = ExchangeRateViewModel()
+    return ExchangeRateViewController(viewModel: dummyViewModel)
 }

909ce7363b76c1110b8abb43730d8df905fb65be
yeseul jang <yyssjj9999@gmail.com>
2026-02-23 17:02:08 +0900

    feat: 뷰모델에 네트워크 의존성 주입

--- currencyConverter/currencyConverter/ViewModel/CurrencyViewModel.swift
+++ currencyConverter/currencyConverter/ViewModel/CurrencyViewModel.swift
@@ -7,18 +7,22 @@
 import Foundation
 
 class CurrencyViewModel {
-    let networkManager = NetworkManager()
+    private let networkingService: Networking
     var selectedCountry = "USD"
     var rates = [String: Double]()
     var upDate: (([Item]) -> Void)?
     var onError: ((String) -> Void)?
     var allItems = [Item]()
     
+    init(networkingService: Networking = NetworkManager()) {
+        self.networkingService = networkingService
+    }
+    
     func fetchCurrencyData() {
         Task {
             do {
                 let result: ExchangeRateResponse =
-                try await networkManager.makeRequest(with: selectedCountry)
+                try await networkingService.makeRequest(with: selectedCountry)
                 
                 if result.rates.isEmpty {
                     onError?("데이터를 불러올 수 없습니다")
